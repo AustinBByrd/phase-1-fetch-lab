33374de8bad357718f94a3942d5869591b13b85e
Austin Byrd <byrda05@gmail.com>
2023-10-05 14:50:00 -0500

    update2

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
+
+
 function fetchBooks() {
-  fetch("https://anapioficeandfire.com/api/books")
+ return fetch("https://anapioficeandfire.com/api/books")
   .then((resp) => resp.json())
-  .then(json => renderBooks(json));
+  .then((json) => renderBooks(json));
 }
 
-function renderBooks(book) {
+function renderBooks(books) {
   const main = document.querySelector("main");
-  book.forEach(book => {
+  books.forEach((book) => {
     const h2 = document.createElement("h2");
     h2.innerHTML = book.name
     main.appendChild(h2);
   });
 }
-
 document.addEventListener("DOMContentLoaded", function() {
   fetchBooks();
+
 });
